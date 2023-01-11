a2253e4a1b5eacaafe6238b46c7a642d9247f6fb
Ethan-Houser <hou21013@byui.edu>
2023-01-11 15:59:55 -0700

    Abstraction Learning Activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        // I like using spacers in the console to make it easier to read.
+        Console.WriteLine("");
+
+        Job job1 = new Job();
+        job1._jobTitle = "Floor Associate";
+        job1._company = "Lowes";
+        job1._startYear = "2019";
+        job1._endYear = "2019";
+        job1.Display();
+
+        Job job2 = new Job();
+        job2._jobTitle = "Maintenance";
+        job2._company = "Aspen Creek Senior Living";
+        job2._startYear = "2022";
+        job2._endYear = "2023";
+        job2.Display();
+
+        Console.WriteLine("");
+
+        Resume resume1 = new Resume();
+        resume1._name = "Ethan Houser";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        resume1.Display();
+
+        Console.WriteLine("");
+
+
     }
 }
