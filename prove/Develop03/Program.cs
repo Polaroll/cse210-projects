fe99d27c45c3f92d683d83f0c376c19c39f25e86
Ethan-Houser <hou21013@byui.edu>
2023-02-15 18:52:49 -0700

    develop 3

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Scripture scripture1 = new Scripture();
+        scripture1.SetText(
+        @"For my soul delighteth in plainness; for after this manner doth the Lord God work among the children of men. For the Lord God giveth light unto the understanding; for he speaketh unto men according to their language, unto their understanding.");
+
+        Reference reference1 = new Reference("2 Nephi", "31", "3");
+
+        string[] text = scripture1.SplitText();
+        foreach (string word in text)
+        {
+            Word word1 = new Word(word);
+            scripture1.AddWords(word1);
+        }
+
+        string usrInput = "go";
+        while (scripture1.IsCompletelyHidden() == false && usrInput != "quit")
+        {
+            scripture1.IsCompletelyHidden();
+            Console.Clear();
+            scripture1.DisplayScripture(reference1.GetReference()); 
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            Console.WriteLine("Press Enter to continue or type 'quit' to finish:");
+            usrInput = Console.ReadLine();
+
+            scripture1.HideWords();
+        }
+
+        Console.Clear();
+        scripture1.DisplayScripture(reference1.GetReference()); 
+        Console.WriteLine("");
+        Console.WriteLine("");
+
+        Console.WriteLine("Press Enter to continue or type 'quit' to finish:");
+        usrInput = Console.ReadLine();
+
     }
 }
